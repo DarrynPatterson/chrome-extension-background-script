aa0d750493c4fc78339ca7dad8ba262abe4ccfdc
Darryn <darryn.patterson@gmail.com>
2019-03-09 22:56:31 +0200

    Removed debugger.

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(receivedMessage);
 
 function receivedMessage(message, sender, sendResponse) {
-  debugger;
   let anchors = document.getElementsByTagName("a");
   for (item of anchors) {
     item.innerHTML = "";
